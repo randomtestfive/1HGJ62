6ba6b194aacd98d45de571124175182b6a806cb7
RandomTestFive <randomtestfive@gmail.com>
2016-07-02 16:56:01 -0400

    Load textures

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 
 public class Entity extends SimulationBody
 {
@@ -14,7 +15,18 @@ public class Entity extends SimulationBody
 	@Override
 	public void render(Graphics2D g2d, double scale)
 	{
+		// save the original transform
+		AffineTransform ot = g2d.getTransform();
+		
+		// transform the coordinate system from world coordinates to local coordinates
+		AffineTransform lt = new AffineTransform();
+		lt.translate(this.transform.getTranslationX() * scale, this.transform.getTranslationY() * scale);
+		lt.rotate(this.transform.getRotation());
+		
+		// apply the transform
+		g2d.transform(lt);
 		g2d.drawImage(Game.tl.textureFromName(this.texturename), 0, 0, null, null);
+		g2d.setTransform(ot);
 	}
 	
 	public void setTextureName(String t)
