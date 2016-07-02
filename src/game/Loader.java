ecd6dcb2b0fa08ffcda352a331d88be7c3839ecb
RandomTestFive <randomtestfive@gmail.com>
2016-07-02 15:55:51 -0400

    Add texture loading stuff

---
+++ src/game/Loader.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.util.ArrayList;
+
+
+public class Loader 
+{
+	protected ArrayList<String> Src;
+	protected ArrayList<String> Name;
+	
+	public Loader()
+	{
+		Src = new ArrayList<String>();
+		Name = new ArrayList<String>();
+	}
+	
+	public void addMap(String src, String name)
+	{
+		Src.add(src);
+		Name.add(name);
+	}
+	
+}
---
+++ src/game/TextureLoader.java
@@ -0,0 +1,51 @@
+package game;
+
+import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+
+public class TextureLoader extends Loader
+{
+	public final int Player = 0;
+	
+	/*private ArrayList<String> textureSrc;
+	private ArrayList<String> textureName;*/
+	//@Override
+	protected Map<String, Image> textureMap;
+
+	public Image[] textures;
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public TextureLoader()
+	{
+		super();
+		textureMap = new HashMap();
+	}
+	
+	/*public void addTexture(String src, String name)
+	{
+		Src.add(src);
+		Name.add(name);
+	}*/
+	
+	public Image textureFromName(String src)
+	{
+		return textureMap.get(src);
+	}
+	
+	public void loadTextures()
+	{
+		textures = new Image[Src.size()];
+		int loadingTexture = 0;
+		for (String src : Src)
+		{
+			textures[loadingTexture] = new ImageIcon(this.getClass().getResource(src)).getImage();
+			textureMap.put(Name.get(loadingTexture), textures[loadingTexture]);
+			new ImageIcon(textures[loadingTexture]);
+			loadingTexture++;
+		}
+	}
+}
